5a010d32e3f2afd53901a50ca0cc32f745629182
dhruv <dhruvagja8808@gmail.com>
2022-02-21 20:55:23 -0700

    small change in jquery

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@
       var hash = this.hash;
 
       // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      // The optional number () specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 1000, function(){
